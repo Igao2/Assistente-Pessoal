d4591411fccc133764d47e83c5bbedd585d4fda6
Aluno Fatec <aluno@fateclab.net>
2022-11-18 11:53:33 -0300

    compromissos screen

--- Helpy/Program.cs
+++ Helpy/Program.cs
@@ -16,7 +16,7 @@ namespace Helpy
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Homepage());
+            Application.Run(new Cadastro());
         }
     }
 }
